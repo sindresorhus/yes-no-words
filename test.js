f02c671c9e27f7e9f5ef354a9756f3a959874236
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-07 14:33:18 +0700

    upgrade to latest AVA

--- test.js
+++ test.js
@@ -5,9 +5,9 @@ test(t => {
 	t.true(x.yes.length > 0);
 	t.true(x.no.length > 0);
 	t.true(x.all.length > 0);
-	t.ok(x.yesRandom());
-	t.ok(x.noRandom());
-	t.ok(x.allRandom());
+	t.truthy(x.yesRandom());
+	t.truthy(x.noRandom());
+	t.truthy(x.allRandom());
 	t.not(x.allRandom(), x.allRandom());
 	t.not(x.all.indexOf('Yes'), -1);
 	t.not(x.all.indexOf('No'), -1);
